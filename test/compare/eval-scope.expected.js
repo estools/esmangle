73dc5f77f02153670b4432fce3eff8bb621752e8
Constellation <utatane.tea@gmail.com>
2012-08-31 17:48:17 +0900

    Add eval scope test

---
+++ test/compare/eval-scope.expected.js
@@ -0,0 +1,1 @@
+(function(){var this_is_too_long_name=20;this_is_too_long_name;eval('')}())
---
+++ test/compare/eval-scope.js
@@ -0,0 +1,5 @@
+(function () {
+  var this_is_too_long_name = 20;
+  this_is_too_long_name;
+  eval('');
+}());
