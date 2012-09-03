b8b6b5f2485c70e7cf8931ac491e93730c172428
Constellation <utatane.tea@gmail.com>
2012-09-04 02:23:59 +0900

    Add more test cases for block removing

--- test/compare/if-else-block-2.expected.js
+++ test/compare/if-else-block-2.expected.js
@@ -1,1 +1,1 @@
-cond?a&&c:v;if(cond){if(a)throw c}else v
+cond?a&&c:v;if(cond){if(a)throw c}else v;if(cond){with(cond)if(a)throw c}else v;if(cond)do if(a)throw c; while(cond); else v
--- test/compare/if-else-block-2.js
+++ test/compare/if-else-block-2.js
@@ -1,2 +1,4 @@
 if (cond) { if (a) c; } else v;
 if (cond) { if (a) throw c; } else v;
+if (cond) { with (cond) if (a) throw c; } else v;
+if (cond) { do if (a) throw c; while (cond) } else v;
