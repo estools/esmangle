b895ce312c15a1d5ba48988f04f583ab08f62247
Constellation <utatane.tea@gmail.com>
2012-09-08 10:07:30 +0900

    Add transform-dynamic-to-static-property pass

--- test/compare.js
+++ test/compare.js
@@ -37,6 +37,7 @@ var fs = require('fs'),
 
 esmangle = require(path.join(root, 'esmangle'));
 passes = [
+    esmangle.require('lib/pass/transform-dynamic-to-static-property-access'),
     esmangle.require('lib/pass/reordering-function-declarations'),
     esmangle.require('lib/pass/remove-unused-label'),
     esmangle.require('lib/pass/remove-empty-statement'),
