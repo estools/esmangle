cb6e3c28a655f0cd6d449b4d0abbdf30f0e33eeb
Constellation <utatane.tea@gmail.com>
2012-09-13 05:57:40 +0900

    Do not reduce directive to sequence expression
    Close #21

---
+++ test/compare/directive-2.js
@@ -0,0 +1,5 @@
+function test() {
+    // We should not reduce them to SequenceExpression
+    'use strict';
+    'use decimal';
+}
---
+++ test/compare/directive-3.js
@@ -0,0 +1,5 @@
+var i = function test() {
+    // We should not reduce them to SequenceExpression
+    'use strict';
+    'use decimal';
+};
