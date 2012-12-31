d4281569b1c9d270c35824f9a97d5d76507cfa20
Constellation <utatane.tea@gmail.com>
2013-01-01 00:58:31 +0900

    Fix invalid drop variable Close #60

---
+++ test/compare/drop-variable-definition-10.js
@@ -0,0 +1,7 @@
+// reported from issue #60
+void function () {
+  var foo;  // this foo should be dropped
+  foo = function () {  // this should be transformed to non-assignment expression
+    return 99;
+  };
+}.call(this);
